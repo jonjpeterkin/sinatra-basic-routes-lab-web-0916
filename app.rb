fdb4567e8b96a67c7eae7c8436e5e41783e14515
stompdoctor <jonjpeterkin@gmail.com>
2016-10-13 21:09:50 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		"Hello, World!"
+	end
+
+	get '/name' do
+		"My name is Jonathan Peterkin"
+	end
+
+	get '/hometown' do
+		"My hometown is Brooklyn NY"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is Some Good Song"
+	end
 end
